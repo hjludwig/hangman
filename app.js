ff8a4c1155fc6275e12a054ae69cc663bd727b54
Jeremy Ludwig <hjeremyludwig@gmail.com>
2020-07-09 19:15:01 -0400

    Added funcitons to check and add guessed letters

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const wordArea = document.querySelector('.word');
 const submitButton = document.querySelector('.submit-button');
 const userInput = document.querySelector('#user-word');
 const inputForm = document.querySelector('.input');
-const wrongGuessArea = document.querySelectorAll('.wrong-guesses');
+const wrongGuessArea = document.querySelector('.wrong-guesses');
+
+
 
 function startPuzzle() {
     let word = userInput.value;
     inputForm.style.display = 'none';
     let lettersArray = word.split("");
+    let lettersGuessed = [];
 
     // Add letters to word area
     lettersArray.forEach(letter => {
@@ -22,19 +25,36 @@ function startPuzzle() {
     function testLetterGuess(event) {
         let guess = event.key;
         let lettersInPlay = document.querySelectorAll('.letter');
-        
+
+        let tester = false;
+
+
         function runTest() {
-            lettersInPlay.forEach(letter => {
-                if (letter.textContent === guess) {
-                    letter.classList.remove('hidden-letter');
+            // Check if guess has already been guessed
+            if (lettersGuessed.indexOf(guess) != -1) {
+                alert("You've already tried that one.");
+            }
+            else {
+                lettersInPlay.forEach(letter => {
+                    if (letter.textContent === guess) {
+                        letter.classList.remove('hidden-letter');
+                        tester = true;
+                    }
+                });
+                if (tester === false) {
+                    let wrongLetter = document.createElement('span');
+                    wrongLetter.textContent = guess;
+                    wrongLetter.classList.add('wrong-letter')
+                    wrongGuessArea.appendChild(wrongLetter);
                 }
-            });
+            }
+
         }
+        console.log(lettersGuessed);
         runTest();
+        lettersGuessed.push(guess);
     }
 }
-
-
 submitButton.addEventListener('click', startPuzzle);
 
 
