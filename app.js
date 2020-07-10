fd3b3537b7a00eff3ce37023be6953e23358b0e1
Jeremy Ludwig <65617794+hjludwig@users.noreply.github.com>
2020-07-10 16:11:48 -0400

    Added tests and refactored

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ const userInput = document.querySelector('#user-word');
 const inputForm = document.querySelector('.input');
 const wrongGuessArea = document.querySelector('.wrong-guesses');
 
+// Get hangman parts
+const head = document.querySelector('.head');
+const body = document.querySelector('.body');
+const leftArm = document.querySelector('.arm-l');
+const rightArm = document.querySelector('.arm-r');
+const leftLeg = document.querySelector('.leg-l');
+const rightLeg = document.querySelector('.leg-r');
 
+const hangmanParts = document.querySelector('.man').children;
 
 function startPuzzle() {
     let word = userInput.value;
     inputForm.style.display = 'none';
     let lettersArray = word.split("");
     let lettersGuessed = [];
+    let counter = 0;
 
     // Add letters to word area
     lettersArray.forEach(letter => {
@@ -27,35 +36,51 @@ function startPuzzle() {
         let lettersInPlay = document.querySelectorAll('.letter');
 
         let tester = false;
-
+        
+        runTest();
+        lettersGuessed.push(guess);
 
         function runTest() {
-            // Check if guess has already been guessed
             if (lettersGuessed.indexOf(guess) != -1) {
                 alert("You've already tried that one.");
             }
             else {
-                lettersInPlay.forEach(letter => {
-                    if (letter.textContent === guess) {
-                        letter.classList.remove('hidden-letter');
-                        tester = true;
-                    }
-                });
+                checkLetter(lettersInPlay, guess);
+
                 if (tester === false) {
-                    let wrongLetter = document.createElement('span');
-                    wrongLetter.textContent = guess;
-                    wrongLetter.classList.add('wrong-letter')
-                    wrongGuessArea.appendChild(wrongLetter);
+                    addWrongLetter(guess);
+                    
+                    hangmanParts[counter].classList.add('show');
+
+                    // Check if player is out of guesses
+                    if (counter === hangmanParts.length -1) {
+                        alert("Out of guesses");
+                    } else {
+                        counter++;
+                    }
+
                 }
             }
-
         }
-        console.log(lettersGuessed);
-        runTest();
-        lettersGuessed.push(guess);
+        function checkLetter(puzzle, letter) {
+            puzzle.forEach(puzzleLetter => {
+                if (puzzleLetter.textContent === letter) {
+                    puzzleLetter.classList.remove('hidden-letter');
+                    tester = true;
+                    return tester;
+                }
+            });
+        }
+        function addWrongLetter(letter) {
+            let wrongLetter = document.createElement('span');
+            wrongLetter.textContent = letter;
+            wrongLetter.classList.add('wrong-letter')
+            wrongGuessArea.appendChild(wrongLetter);
+        }
     }
 }
 submitButton.addEventListener('click', startPuzzle);
+
 
 
 
